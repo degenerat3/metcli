2c48516e9beedd27ddfdfd5daf7311d5375a0942
degenerat3 <james.maskelony@gmail.com>
2019-10-08 22:25:39 -0400

    export some more functions

--- payload.go
+++ payload.go
@@ -5,16 +5,16 @@ import (
 	"strings"
 )
 
-// base64 the payload and prepend/append magic chars
-func encodePayload(data string, mode string, aid string, magicstr string, magictermstr string) string {
+// EncodePayload will base64 the payload and prepend/append magic chars
+func EncodePayload(data string, mode string, aid string, magicstr string, magictermstr string) string {
 	preEnc := mode + "||" + aid + "||" + data
 	encStr := base64.StdEncoding.EncodeToString([]byte(preEnc))
 	fin := magicstr + encStr + magictermstr
 	return fin
 }
 
-//decode from MAD into plaintext string
-func decodePayload(payload string, magicstr string, magictermstr string) string {
+//DecodePayload will decode from MAD into plaintext string
+func DecodePayload(payload string, magicstr string, magictermstr string) string {
 	encodedPayload := strings.Replace(payload, magicstr, "", -1) //trim magic chars from payload
 	encodedPayload = strings.Replace(encodedPayload, magictermstr, "", -1)
 	data, err := base64.StdEncoding.DecodeString(encodedPayload)
